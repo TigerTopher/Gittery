909e143e270e5218a54c131920b5ca73a187acc0
Topher <christov9999@gmail.com>
2015-04-10 18:29:51 +0800

    With Destroy already

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -2,7 +2,10 @@ class ArticlesController < ApplicationController
 	def index
 		@articles = Article.all
 	end
-
+	
+	def show
+		@article = Article.find(params[:id])
+	end
 
 	def new
 		@article = Article.new
@@ -32,10 +35,12 @@ class ArticlesController < ApplicationController
 		end
 	end
 
-	def show
+	def destroy
 		@article = Article.find(params[:id])
-	end
+		@article.destroy
 
+		redirect_to articles_path
+	end
 
 	private
 		def article_params
